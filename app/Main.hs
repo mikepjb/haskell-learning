9ca33d7a40abfa9de4b89abce124d422db6c96c8
Mike Bruce <mikepjb@proton.me>
2024-04-26 15:58:06 +0100

    zlib problem, trying to create a servant server

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,34 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
+
+import GHC.Generics
+import Data.Time.Calendar
+import Data.Aeson
+import Data.Aeson.Types
+import Servant
+
+type MessageAPI = "messages" :> Get '[JSON] [Message]
+
+data Message = Message
+    { user :: String
+    , message :: String
+    , created_at :: Day
+} deriving (Eq, Show, Generic)
+
+messages :: [Message]
+messages =
+    [ Message "oli1" "hello!" (fromGregorian 1683 3 1)
+    , Message "oli2" "hello there other oli!!" (fromGregorian 1683 3 1)
+    ]
+
+server :: Server 
 
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
--- shell.nix
+++ shell.nix
@@ -1,10 +1,19 @@
 let
   myNixPkgs = import <nixpkgs> {};
 in
-myNixPkgs.mkShell {
+myNixPkgs.mkShell rec {
   nativeBuildInputs = with myNixPkgs; [
     cabal-install # terminal app cabal
     ghc # Haskell compiler
     haskell-language-server # Haskell LSP
+    zlib
+    haskellPackages.zlib
+    zstd
+    haskellPackages.zstd
+    lzma
+    bzip2
+    haskellPackages.bz2
   ];
+
+  LD_LIBRARY_PATH = myNixPkgs.lib.makeLibraryPath nativeBuildInputs;
 }
