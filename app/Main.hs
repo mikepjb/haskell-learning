228b84b117bf47aed53b0036e403b11bef0b4f3f
Mike Bruce <mikepjb@proton.me>
2024-04-26 17:31:39 +0100

    we did something! (see /messages endpoint)

--- app/Main.hs
+++ app/Main.hs
@@ -10,9 +10,9 @@ module Main where
 
 import GHC.Generics
 import Data.Time.Calendar
-import Data.Aeson
-import Data.Aeson.Types
 import Servant
+import Data.Aeson
+import Network.Wai.Handler.Warp
 
 type MessageAPI = "messages" :> Get '[JSON] [Message]
 
@@ -22,13 +22,22 @@ data Message = Message
     , created_at :: Day
 } deriving (Eq, Show, Generic)
 
+instance ToJSON Message
+
 messages :: [Message]
 messages =
     [ Message "oli1" "hello!" (fromGregorian 1683 3 1)
     , Message "oli2" "hello there other oli!!" (fromGregorian 1683 3 1)
     ]
 
-server :: Server 
+server :: Server MessageAPI
+server = return messages
+
+messageAPI :: Proxy MessageAPI
+messageAPI = Proxy
+
+app :: Application
+app = serve messageAPI server
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = run 8081 app
